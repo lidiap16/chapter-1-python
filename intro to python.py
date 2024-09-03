39e76a00a642f5d7f9956320467fda5c1ee29949
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-09-03 13:44:59 +0100

    Add files via upload

---
+++ intro to python.py
@@ -0,0 +1,37 @@
+#author: Lidia Pasiecznik, 02/09/24
+#intro to python("hello world")
+print("hello world")
+
+
+
+num = float(21.0)
+print (num)
+print(9*7)
+
+
+age = input("please enter your age: ")
+age = int(age)
+print("your are", age, "years old!")
+
+
+
+answer = input("do you have a loyalty card? (Y/N)")
+if answer == "Y" :
+    print("please scan your card.")
+    
+    
+print("*    *")
+
+print(" ****")
+
+
+print("A\tB\nC")
+print("C:\\Users\\johndoe\\Documents\\myfile.txt")
+
+
+print("\t   ")
+
+
+
+
+    
