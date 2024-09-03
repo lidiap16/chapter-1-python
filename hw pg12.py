66f07dcf913c0dc053b84f07a464022ec7ba36ea
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-09-03 14:03:13 +0100

    Add files via upload

---
+++ hw pg12.py
@@ -0,0 +1,23 @@
+#author: Lidia Pasiecznik hw 9/03/24
+
+
+print("Goodbye!", end = " ")
+print("Goodbye!")
+
+print("no \tway" , ".")
+
+print("* *")
+print(" * ")
+print("* *")
+
+print("\n $ $")
+print(" \_/ ")
+
+print("\n  ^  ")
+print(" / \ ")
+print("_____")
+
+print(" \n ** ")
+print("*  *")
+print(" ** ")
+
