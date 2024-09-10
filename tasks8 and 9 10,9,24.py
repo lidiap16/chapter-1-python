261853c1479a5e4f24ed7f12680323fcd2c22b91
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-09-10 18:11:52 +0100

    Add files via upload

---
+++ tasks8 and 9 10,9,24.py
@@ -0,0 +1,24 @@
+#lidia pasiecznik 10/9/24
+#task 9
+
+
+runningTotal = 0
+price1 = 10
+runningTotal = runningTotal + price1
+
+price2 = 14
+runningTotal = runningTotal + price2
+price3 = 6
+runningTotal = runningTotal + price3
+
+print("Total amount is", runningTotal)
+#you can not reorder the code
+
+#task 10
+
+"""
+1. to give a value for the numbers it'll go between by
+2. its because the high number has to be higher than low
+3. the computer has to add all of them first
+4. yes , running total would be , ni = 0 , ni = ni +n2, nni = ni+ n3 ...
+"""
