53c5b83396d37b392fd348be5422dc34f24e90d5
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-09-10 18:48:38 +0100

    Add files via upload

---
+++ hw tasks1,5 10,9,24.py
@@ -0,0 +1,44 @@
+#lidia pasiecznik
+# hw tasks in book
+
+#task 1
+age = 16
+print(age, "what a great age")
+
+#task2
+a = 1.9
+b = 5.5
+print(a + b)
+print(a - b)
+print(a * b)
+
+#task3
+num1 = 5.5
+num2 = 2.3
+
+average = (num1 + num2) / 2
+
+remainder = num1 % num2
+
+power = num1 ** num2
+
+print( num1, num2)
+print(average)
+print(remainder)
+print(power)
+
+#task4
+fahrenheit = 87
+celsius = 5/9 * fahrenheit - 23
+celsius = round(celsius, 2)
+print(celsius, "is the temp in C")
+
+
+#task5
+
+distance_miles = 7074
+distanceKm = (distance_miles/ 1.60935)
+distanceKm = round(distanceKm,0)
+print(distanceKm)
+cost =  (distanceKm * 900)
+print("your flight costs", distanceKm, "euro")
