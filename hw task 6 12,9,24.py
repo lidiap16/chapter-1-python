69d280a94313babe559c85caffeaf09162de8220
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-09-12 19:40:30 +0100

    Add files via upload

---
+++ hw task 6 12,9,24.py
@@ -0,0 +1,12 @@
+#lidia pasiecznik 12/9/24
+#hw task 6
+
+h = 5
+r = 2
+pi = 3.14
+volume = (pi * r**2 * h)
+print(volume)
+totalLiquid = 12090
+numCylinder = (totalLiquid / volume)
+numCylinder = round(numCylinder, 0)
+print(numCylinder)
