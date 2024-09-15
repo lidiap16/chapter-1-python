2a492308e67fc1536828f0121b3b1a826b087a56
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-09-15 13:24:03 +0100

    Add files via upload

--- hw task2.py
+++ hw task2.py
@@ -15,7 +15,7 @@ while player == False:
     print("\t|                               |")
     option = input("\t| choose option : ")
     if option == "1":
-        print("\t|september\t\t\t|")
+        print("\t|septembeR¬   to exit press E\t|")
         print("\t|  M  T  W  TH F  S  Su\t\t|")
         print("\t|\t             1\t\t| ")
         print("\t|  2  3  4  5  6  7  8\t\t| ")
@@ -24,24 +24,42 @@ while player == False:
         print("\t|  23 24 25 26 27 28 29\t\t|")
         print("\t|  30\t\t\t\t|")
         print("\t|-------------------------------|")
+        addEvent = input("\t|")
+        if addEvent == "E":
+            player == False
+
     if option == "2":
-        print("\t| TASKS\t\t\t\t|")
+        print("\t| TASKS¬  to exit press E\t|")
         print("\t| 1, finish homework\t\t|")
         print("\t| 2, study\t\t\t|")
         print("\t|-------------------------------|")
+        addEvent = input("\t|")
+        if addEvent == "E":
+            player == False
+
     if option == "3":
-        print("\t| REMINDERS\t\t\t|")
+        print("\t| REMINDERS¬ to exit press E\t|")
         print("\t| 1, lidia's bday on the 22 oct\t|")
         print("\t| 2, team party in a week 28 sep|")
         print("\t|-------------------------------|")
+        addEvent = input("\t|")
+        if addEvent == "E":
+            player == False
     if option == "4":
-        print("\t| 1, soccer part\t\t|")
-        
+        print("\t| events¬   to exit press E\t|")
+        print("\t| 1, halloween\t\t\t|")
+        print("\t| 2, exams\t\t\t|")
+        print("\t|-------------------------------|")
+        addEvent = input("\t|")
+        if addEvent == "E":
+            player == False
+
     if option == "5":
         print("\t| add event ¬", "or to exit press E")
         addEvent = input("\t| 1.")
         if addEvent == "E":
             player == False
+        
     if option == "6":
         print("\t| add task ¬", "or to exit press E")
         addEvent = input("\t| 1.")
