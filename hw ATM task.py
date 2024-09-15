0bbaf17db7f3fb76c0ab8185abf8cbc92d97a079
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-09-15 11:50:05 +0100

    Add files via upload

---
+++ hw ATM task.py
@@ -0,0 +1,30 @@
+# Date: Sept 2024
+# Author: MS Hayes
+# Purpose: This code displays an ATM menu
+
+print("\t|-------------------------------|")
+print("\t|\t LCCS BANK LIMITED\t|")
+print("\t|\t ATM Main Menu\t\t|")
+print("\t|\t\t\t\t|")
+print("\t|\t1. Balance Enquiry\t|")
+print("\t|\t2. Cash Lodgement\t|")
+print("\t|\t3. Cash Withdrawal\t|")
+print("\t|\t4. Cash Transfer\t|")
+print("\t|\t5. Change PIN\t\t|")
+print("\t|\t6. Other Services\t|")
+
+print("\t|\t7. Exit\t\t\t|")
+print("\t|-------------------------------|")
+print("\n\t|-------------------------------|")
+print("\t|\t\t\t\t|")
+option = input("\t| CHOOSE AN OPTION >> ")
+if option == "1":
+    print("\t|-------------------------------|")
+    print("\t| your balance is 1,234\t\t|")
+    print("\t|-------------------------------|")
+      
+if option == "3":
+    print("\t|-------------------------------|")
+    print("\t|you can only withdraw: 500\t|")
+    print("\t|-------------------------------|")
+
