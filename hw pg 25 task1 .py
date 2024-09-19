9c74ddeb113e47f7b6233afded4aa845006b5f90
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-09-19 20:09:06 +0100

    Add files via upload

---
+++ hw pg 25 task1 .py
@@ -0,0 +1,7 @@
+#lidia pasiecznik   19.09.24
+#hw pg 25
+
+favouriteArtist = input("enter the name of your favourite artist: ")
+complement = " is brilliant."
+result = favouriteArtist + complement
+print(result)
