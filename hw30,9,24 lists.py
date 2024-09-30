f371cf85467429b50a6905b92b9bb8f22dab4625
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-09-30 20:43:02 +0100

    Add files via upload

---
+++ hw30,9,24 lists.py
@@ -0,0 +1,13 @@
+#lidia pasiecznik 30/09.24
+#hw tasks
+
+#task 3
+fruits = ['strawberry', 'lemon', 'orange', 'raspberry', 'cherry']
+print(fruits)
+fruits[0] = 'apple'
+fruit = 'melon'
+fruits[1] = fruit
+fruits[2] = 'raspberry'
+fruits[3] = fruits[4]
+fruits[4] = 'pineapple'
+print(fruits)
