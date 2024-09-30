5109fe087f3f453d8d2fc61c4d727b7aa16ec0b9
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-09-30 21:02:42 +0100

    Add files via upload

--- hw30,9,24 lists.py
+++ hw30,9,24 lists.py
@@ -11,3 +11,22 @@ fruits[2] = 'raspberry'
 fruits[3] = fruits[4]
 fruits[4] = 'pineapple'
 print(fruits)
+
+#task 4
+fruits = ['strawberry', 'lemon', 'orange', 'raspberry', 'cherry']
+print(fruits[0])
+print(fruits[3])
+print(fruits[2])
+print(fruits[len(fruits)-1])
+print(fruits[1])
+
+fruit = fruits[2+2]
+print(fruit)
+print(fruit[0])
+
+orange = fruits[1]
+print(orange)
+lemon = fruits[1]
+print(lemon)
+
+print(fruits[0][0]+fruits[1][0]+fruits[2][0])
