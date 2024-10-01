2cc7e5d0fa18ac46a855ff8f0d7ceabe9f28a0cc
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-10-01 21:12:19 +0100

    Add files via upload

--- hw list tasks.py
+++ hw list tasks.py
@@ -29,15 +29,24 @@ list2 = (panList[4:])
 list2.reverse()
 print(list2+list1)
 
+#book pg 43 task 3
+listrain = []
+for i in range(7):
+    rain = int(input(f"enter rain every day {i+1}: "))
+    listrain.append(rain)
+print(listrain)
+print(listrain[0]+listrain[1]+listrain[2]+listrain[3]+listrain[4]+listrain[5]+listrain[6], "cm of rain over the week")
+print((listrain[0]+listrain[1]+listrain[2]+listrain[3]+listrain[4]+listrain[5]+listrain[6]) / 7)
+
 
 #task 9
 
-import random
-fruitFile = open("fruits.txt","r")
-#fruitFile = ["cherry", "melon", "banana", "pineapple", "strawberry"]
-fileContents = fruitFile.read()
-fruitFile.close()
-fruits = fileContents.split()
-print(random.choice(fruits))
-print(random.choice(fruits))
-print(random.choice(fruits))
+# import random
+# fruitFile = open("fruits.txt","r")
+# #fruitFile = ["cherry", "melon", "banana", "pineapple", "strawberry"]
+# fileContents = fruitFile.read()
+# fruitFile.close()
+# fruits = fileContents.split()
+# print(random.choice(fruits))
+# print(random.choice(fruits))
+# print(random.choice(fruits))
