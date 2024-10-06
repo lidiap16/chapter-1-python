afd5fdb3b2ba28695c4243829c170a68831f3612
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-10-06 14:15:04 +0100

    Add files via upload

---
+++ hw tasks pg =30 .py
@@ -0,0 +1,30 @@
+#lidia pasiecznik 6/10/24
+#tasks book hw
+
+
+#task1
+num1 = int(input("enter number"))
+num2 = int(input("enter number"))
+
+if num2 % num1 == 0: # use % => modulus  remainder ?
+    print("divides")
+else:
+    print("doesnt divide")
+    
+    
+#task2
+        
+age = int(input("enter age"))
+if age >= 17:
+    print("you are eligible")
+else:
+    print("your are not old enough")
+
+""" 
+                                    / equal -> print " you are eligible "
+                                   /
+enter age-> if age grater or equal<
+                                   \
+                                    \ else -> print " you are not old enough "
+"""
+                                
