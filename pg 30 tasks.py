6657ab6ca016d698e24ad7896d604799e24307f4
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-10-08 14:06:49 +0100

    Add files via upload

---
+++ pg 30 tasks.py
@@ -0,0 +1,14 @@
+#lidia pasiecznik 8/10/24
+#book tasks pg 30
+
+#task 3
+
+item = int(input("enter cost of item: "))
+if item > 10000:
+    print("go to tender")
+elif item >= 500 and item <= 10000:
+    print("get quotes from threedifferent suppliers")
+else:
+    print("order item")
+    
+#task 4
