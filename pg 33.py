5b5572173adc107f9ec0038e9b87c35fdaaf0a34
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-10-15 14:18:31 +0100

    Add files via upload

---
+++ pg 33.py
@@ -0,0 +1,89 @@
+#lidia pasiecznik
+#13/10/24 hw qs pg 33
+
+#task 1 # adds numbers between 1000 to 1500
+# start = 1000
+# end = 1500
+# total = 0
+# current = start
+# while current <= end:
+#     total += current
+#     current += 1
+#     
+# print(total)
+# 
+# #task 2 # takes 6 nums and divides by 6 fo average
+# 
+# count = 0
+# total = 0
+# while count < 6:
+#     number = float(input("enter a number:"))
+#     total += number
+#     count += 1
+# average = total / 6
+# print(average)
+# print(round(average))
+# 
+# #task 3
+# password = False
+# while password == False:
+#     name = str(input("enter name: "))
+#     if name == "Maureen":
+#         print("welcome Maureen")
+#         break;
+#     else:
+#         print("wrong")
+# while password == False:
+#     passnum = input("enter password")
+#     if passnum == "1234":
+#         print("welcome")
+#         break;
+#     else:
+#         print("access denied")
+
+
+
+#task 4
+# num = 1
+# eventotal = 0
+# oddtotal = 0
+# limit = int(input("enter num greater that 0: "))
+# while num <= limit:
+#     if num % 2 == 0: # modulous if = to zero even number + 1 
+#         eventotal += num
+#     else:
+#         oddtotal += num #else add total to num
+#     num += 1
+# print(eventotal)
+# print(oddtotal)
+# 
+# #task 5
+# average = 0
+# number = True
+# number2 = 0
+# while number:
+#     NumINPUT = int(input("enter num"))
+#     if NumINPUT < 1:
+#         number = False
+#         break
+#     average += NumINPUT
+#     number2 += 1
+# average = average/number2
+# print(average)
+
+#task 6
+# 
+# sentence = input("enter words")
+# wordcount = sentence.count(" ")
+# print(wordcount+1)
+
+#task7
+print("**************************")
+print("| 1. curtains            |")
+print("| 2. cushion covers      |")
+print("| 3. quilts              |")
+print("| 4. exit                |")
+print("**************************")
+
+
+     
