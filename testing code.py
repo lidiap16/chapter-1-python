7a41c6b279670905846df10ad488d8314edc141b
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-10-19 23:16:43 +0100

    Add files via upload

--- testing code.py
+++ testing code.py
@@ -4,10 +4,25 @@
 #Date:
 # Description: Working with test cases
 
-radius = float(input("Enter radius: "))
-volume = (4/3*3.14*radius**3)
-print("The volume is: ", volume)
-print(round(volume,2))
+# radius = float(input("Enter radius: ")) 
+# if radius > 0:
+#     volume = (4/3*3.14*radius**3)
+#     print("The volume is: ", volume)
+#     print(round(volume,2))
+# else:
+#     radius == str
+#     print("invalid character")
+while True:   #start of loop
+    try:  #tryes the code
+        radius = float(input("enter a positive radius: ")) #asks the user to input the radius, attempting to convert the input to a float
+        if radius > 0: # checks for positive number
+            break #exits the loop
+        else:
+            print("please enter a positive number") #prompt for positive value
+    except ValueError: 			#handle the case where the input is not a number
+        print("invalid input. please enter a positive number")
+volume = (4/3*3.14*radius**3) #calculates the volume
+print("the volume is:", round(volume, 2)) #prints rounded volume
 """
 test run 1
 ***************************************************************
@@ -35,16 +50,16 @@ test run 1
 #     #                  #          #RESULT    #result  # y/n
 ***************************************************************
 """
-#6  #test a regular value #6        #904.32    #
+#6  #test a regular value #6        #904.32    #904.32 #yes
 #**************************************************************
-#2  #test a large number  #4096     #2.877055  #  
+#2  #test a large number  #4096     #2.877055  #287705542601.39 #yes  
                                     #10^11
 #**************************************************************
-#3  #test a negative no   #-2       #imput     # 
-                                    #invalid
+#3  #test a negative no   #-2       #prompts      #prompts #yes
+                                    #again         again 
 #**************************************************************
-#4  #test dcimal          #2.7      #82.40616  #
+#4  #test dcimal          #2.7      #82.40616     #82.41  #no
 #**************************************************************
-#5  #test 0               #0        #0         # 
+#5  #test 0               #0        #prompts again #prompts again #yes
 #**************************************************************
-#6  #test input string    #hello    #errror    #
+#6  #test input string    #hello    #prompts again #prompts again #yes
