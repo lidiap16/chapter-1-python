948035180aa01b25ed99564777571d3e0c27fba9
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-11-03 21:57:09 +0000

    Add files via upload

--- hw midterm decimal to binary.py
+++ hw midterm decimal to binary.py
@@ -7,3 +7,8 @@ while decimalInput > 0: #while loop while number is grater that 0
     binaryOutput = str(remainder) + binaryOutput #gets remainder put it to string; so no math and adds (1 or 0) to the start of the string
     decimalInput = decimalInput // 2 #updates the num by dividing by 2
 print(binaryOutput) # prints output
+
+#
+binary_input = input("enter binary code: ")
+decimal_output = int(binary_input, 2) # converts the binary string to a decimal intiger. the 2 specifies that the input is in base 2
+print(decimal_output)
