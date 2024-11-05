f21d40c33ac3a166d7bb6061ab5f98c2c8aa3d32
lidiap16 <ctw20.lidia.pasiecznik@corketb.ie>
2024-11-05 17:34:26 +0000

    Add files via upload

---
+++ hex to decimal.py
@@ -0,0 +1,16 @@
+binary_input = input("enter hex code: ")
+decimal_output = int(binary_input, 16) # converts the binary string to a decimal intiger. the 2 specifies that the input is in base 2
+print(decimal_output)
+# remainder = binary_input
+# if remainder == 10:
+#     binaryOutput = "A" + binaryOutput
+# elif remainder == 11:
+#     binaryOutput = "B" + binaryOutput
+# elif remainder == 12:
+#     binaryOutput = "C" + binaryOutput
+# elif remainder == 13:
+#     binaryOutput = "D" + binaryOutput
+# # elif remainder == 14:
+# #     binaryOutput = "E" + binaryOutput
+# # elif remainder == 15:
+# #     binaryOutput = "F" + binaryOutput
